6005f6d665de534b1d56dc3dcea5708ebf6f81a0
Sebastian Bernauer <sebastian.bernauer@stackable.de>
2024-04-22 22:03:37 +0200

    let script take arg for file (helpful for mtultiple machines)

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,7 +1,9 @@
 use std::{collections::HashSet, path::Path};
 
 pub fn main() {
-    let input_path = "/home/sbernauer/Desktop/private/1brc/1brc/measurements.txt";
+    let args: Vec<String> = std::env::args().collect();
+    let input_path: &String = args.get(1).expect("Need two args: input and output file");
+    let expected_out_path = args.get(2).expect("Need two args: input and output file");
 
     let out = obrc_rs::solution(Path::new(input_path));
 
@@ -16,13 +18,12 @@ pub fn main() {
     let three_digs: HashSet<_> = names.iter().map(|n| n[..3].to_owned()).collect();
     assert_eq!(three_digs.len(), names.len());
 
-    check(out);
+    check(out, expected_out_path);
 }
 
-fn check(out: Vec<obrc_rs::ProcessedStation>) {
+fn check(out: Vec<obrc_rs::ProcessedStation>, expected_out_path: &str) {
     let formatted = obrc_rs::format_results(&out);
 
-    let expected_out_path = "/home/sbernauer/Desktop/private/1brc/1brc/expected_out.txt";
     let expected = std::fs::read_to_string(expected_out_path).unwrap();
     let expected = expected.trim();
     pretty_assertions::assert_eq!(formatted, expected);
